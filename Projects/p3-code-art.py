56d8586a71cfb3fd0275a024d59b166f6930f496
bagelman1212 <briansanford@seattleacademy.org>
2025-04-09 17:53:55 +0000

    project 3 resubmission with comments

--- Projects/p3-code-art.py
+++ Projects/p3-code-art.py
@@ -3,16 +3,19 @@ import turtle
 t = turtle.Turtle()
 t.penup()
 t.color("LawnGreen")
+#This changes the background color
 turtle.Screen().bgcolor("Black")
 t.pendown()
 
+#This selects the colors that will be displayed
 colors = ["LawnGreen","Teal","Orange"]
+#This shows how many times it will be repeated
 for i in range(500):
     t.color( colors[i % 3] )
     t.speed(10 + 1)
     t.forward(100 + i)
-    t.left(276)
+    t.left(220)
     t.forward(100)
-    t.right(76)
+    t.right(20)
 
 turtle.exitonclick()
